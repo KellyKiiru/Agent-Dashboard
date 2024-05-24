d86d61322a5726f8f60e68b0794c4410b1363adf
kellykiiru <infowithkiiru@gmail.com>
2024-05-24 23:44:15 +0300

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { TashboardComponent } from './tashboard/tashboard.component';
 import { SchoolDetailsComponent } from './schools/school-details/school-details.component';
 import { SchoolsListComponent } from './schools/schools-list/schools-list.component';
-
+import { InvoicesListComponent } from './invoices-list/invoices-list.component';
 
 const routes: Routes = [
   { path: 'tashboard', component: TashboardComponent },
   { path: 'school-details', component: SchoolDetailsComponent },
   { path: 'school-list', component: SchoolsListComponent },
   { path: 'school-details/:id', component: SchoolDetailsComponent },
+  { path: 'invoices', component: InvoicesListComponent },
 ];
 
 @NgModule({
