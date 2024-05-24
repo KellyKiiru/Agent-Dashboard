4d1d36833967cd9d7446b62a6a2210ac76e3773a
kellykiiru <infowithkiiru@gmail.com>
2024-05-24 22:05:35 +0300

    update school-list component to fetch and dispay schools

--- src/app/schools/schools-list/schools-list.component.ts
+++ src/app/schools/schools-list/schools-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { School } from 'src/app/interfaces/school.interface';
+import { SchoolService } from 'src/app/services/school.service';
 
 @Component({
   selector: 'app-schools-list',
   templateUrl: './schools-list.component.html',
   styleUrls: ['./schools-list.component.css']
 })
-export class SchoolsListComponent {
+export class SchoolsListComponent implements OnInit {
+  schools: School[] = [];
 
+  constructor(private schoolService: SchoolService) { }
+
+  ngOnInit(): void {
+    this.schoolService.getSchools().subscribe(schools => {
+      this.schools = schools;
+    });
+  }
 }
