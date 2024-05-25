48fbc0dee35612d0bda10db2f91c935ed569a32b
kellykiiru <infowithkiiru@gmail.com>
2024-05-25 09:49:13 +0300

    update school service

--- src/app/services/school.service.ts
+++ src/app/services/school.service.ts
@@ -1,30 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class SchoolService {
+
   private apiUrl = 'http://localhost:3000';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getSchools(): Observable<any> {
     return this.http.get(`${this.apiUrl}/schools`);
   }
 
-  getInvoices(): Observable<any> {
-    return this.http.get(`${this.apiUrl}/invoices`);
-  }
-
-
-  getInvoicesById(schoolId: number): Observable<any> {
-    return this.http.get(`${this.apiUrl}/invoices?schoolId=${schoolId}`);
-  }
-  
   getCollections(): Observable<any> {
     return this.http.get(`${this.apiUrl}/collections`);
   }
@@ -33,28 +23,32 @@ export class SchoolService {
     return this.http.get(`${this.apiUrl}/collections?schoolId=${schoolId}`);
   }
 
-  countFinanceSignUps(): Observable<number> {
-    return this.getSchools().pipe(
-      map((schools: any[]) => {
-        return schools.filter(school => school.products.includes('Zeraki Finance')).length;
-      })
-    );
+  getSignups(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/signups`);
   }
 
-  countAnlysisSignups(): Observable<number> {
-    return this.getSchools().pipe(
-      map((schools: any[]) => {
-        return schools.filter(school => school.products.includes('Zeraki Analytics')).length;
-      })
-    );
+  getRevenue(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/revenue`);
   }
 
-  countTimetableSignups(): Observable<number> {
-    return this.getSchools().pipe(
-      map((schools: any[]) => {
-        return schools.filter(school => school.products.includes('Zeraki Timetable')).length;
-      })
-    );
+  getInvoices(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/invoices`);
   }
+    
+  getInvoicesById(schoolId: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/invoices?schoolId=${schoolId}`);
+  }
+
+
+  async getSchoolName(schoolId: number): Promise<string> {
+    try {
+      const schoolsData = await this.getSchools().toPromise();
+      const school = schoolsData.find((school: { id: number; }) => school.id === schoolId);
+      return school ? school.name : 'Unknown';
+    } catch (error) {
+      console.error('Error fetching school data:', error);
+      return 'Unknown';
+    }
+  }
+
 }
-
