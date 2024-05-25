d92bd528dbfc15fdef2954e1c65af7abfe7f7ec1
kellykiiru <infowithkiiru@gmail.com>
2024-05-25 18:59:02 +0300

    delete unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CollectionsComponent } from './collections/collections.component';
-import { SignUpsComponent } from './sign-ups/sign-ups.component';
-import { TotalRevenueComponent } from './total-revenue/total-revenue.component';
-import { ChequesComponent } from './cheques/cheques.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { TopMetricsComponent } from './dashboard/top-metrics/top-metrics.component';
-import { TargetsVisualizationComponent } from './dashboard/targets-visualization/targets-visualization.component';
-import { SignupsOverviewComponent } from './dashboard/signups-overview/signups-overview.component';
-import { UpcomingInvoicesComponent } from './dashboard/upcoming-invoices/upcoming-invoices.component';
 import { SchoolsListComponent } from './schools/schools-list/schools-list.component';
 import { SchoolDetailsComponent } from './schools/school-details/school-details.component';
 import { InvoicesComponent } from './schools/invoices/invoices.component';
@@ -29,22 +21,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
-    CollectionsComponent,
-    SignUpsComponent,
-    TotalRevenueComponent,
-    ChequesComponent,
-    NavigationComponent,
-    TopMetricsComponent,
-    TargetsVisualizationComponent,
-    SignupsOverviewComponent,
-    UpcomingInvoicesComponent,
     SchoolsListComponent,
     SchoolDetailsComponent,
     InvoicesComponent,
     MyChartComponent,
     TashboardComponent,
     InvoicesListComponent,
-    SideNavComponent
+    SideNavComponent,
+    CollectionsComponent
   ],
   imports: [
     BrowserModule,
