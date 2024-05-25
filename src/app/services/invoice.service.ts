0f0aa309c870872a8848e4a8eb2b4cdda2ed6ece
kellykiiru <infowithkiiru@gmail.com>
2024-05-25 21:24:33 +0300

    docs ready for deployment

--- src/app/services/invoice.service.ts
+++ src/app/services/invoice.service.ts
@@ -7,7 +7,9 @@ import { Invoice } from '../interfaces/invoice.interface';
   providedIn: 'root'
 })
 export class InvoiceService {
-  private apiUrl = 'http://localhost:3000';
+  // private apiUrl = 'http://localhost:3000';
+
+  private apiUrl = 'https://kellykiiru.github.io/Agent-Dashboard.github.io'; 
 
   constructor(private http: HttpClient) { }
 
